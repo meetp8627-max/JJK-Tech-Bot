fe8bae4dfd2e0a4957271aca96da18a5b4d682c6
meetp8627-max <meetp8627@gmail.com>
2026-03-14 14:54:13 +0530

    Update X LEAKS (VIP) RSS feed URL

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ RSS_FEEDS = {
     "🎌 OTAKU UPDATE": "https://www.cbr.com/feed/category/anime/",
     "🎮 GAMING ZONE": "https://news.google.com/rss/search?q=Free+Fire+MAX+OB+update+leaks+today&hl=en-IN&gl=IN&ceid=IN:en",
     "🤖 ANDROID NEWS": "https://www.androidauthority.com/feed/"
-    "🐦 X LEAKS (VIP)": "https://rss.app/feeds/xsSeQZ1fyB1tRCpZ.xml" # <-- Naya Addition!
+    "🐦 X LEAKS (VIP)": "https://rss.app/feeds/EeMhHchJsgWa4O1T.xml" # <-- Naya Addition!
 }
     
 KEYWORDS = ["poco", "free fire", "gojo", "jujutsu kaisen", "hyperos", "xiaomi", "redmi", "ob update", "iphone"]
